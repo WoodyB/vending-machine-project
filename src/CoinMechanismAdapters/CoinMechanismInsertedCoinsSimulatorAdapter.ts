8c8aa3c7db90e53beb4eb0abc95a219a807302e4
Woody Beverley <woodybeverley@gmail.com>
2024-03-23 16:20:30 -0700

    Improved OOP in CoinMechanismInsertedCoinsSimulatorAdapter and added missing unit tests for it

--- src/CoinMechanismAdapters/CoinMechanismInsertedCoinsSimulatorAdapter.ts
+++ src/CoinMechanismAdapters/CoinMechanismInsertedCoinsSimulatorAdapter.ts
@@ -1,28 +1,23 @@
-/* 
-** Coin Mechanism Adapter for our simulator
-*/
-
-import { CoinMechanismInsertedCoinsInterface } from '../interfaces'
-import {Coins } from '../types';
+import { CoinMechanismInsertedCoinsInterface, CoinHandlerInterface } from '../interfaces'
+import { Coins } from '../types';
 
 export class CoinMechanismInsertedCoinsSimulatorAdapter implements CoinMechanismInsertedCoinsInterface{
   private pendingTransactionTotal: number;
+  private coinHandlers: Map<Coins, CoinHandlerInterface>;
 
   constructor() {
     this.pendingTransactionTotal = 0;
+    this.coinHandlers = new Map<Coins, CoinHandlerInterface>();
+    this.coinHandlers.set(Coins.QUARTER, new QuarterHandler);
+    this.coinHandlers.set(Coins.DIME, new DimeHandler);
+    this.coinHandlers.set(Coins.NICKEL, new NickelHandler);
   }
 
   public insertCoin(coin: Coins): void {
-    if (coin === Coins.QUARTER) {
-      this.pendingTransactionTotal += .25;
-    }
-
-    if (coin === Coins.DIME) {
-      this.pendingTransactionTotal += .10;
-    }
-    
-    if (coin === Coins.NICKEL) {
-      this.pendingTransactionTotal += .05;
+    //console.log(`DEBUG: Called insertCoin(${coin})`);
+    const coinHandler = this.coinHandlers.get(coin);
+    if (coinHandler) {
+      this.pendingTransactionTotal = coinHandler.handleCoin(this.pendingTransactionTotal);
     }
   }
 
@@ -30,3 +25,21 @@ export class CoinMechanismInsertedCoinsSimulatorAdapter implements CoinMechanism
     return this.pendingTransactionTotal;
   }
 }    
+
+class QuarterHandler implements CoinHandlerInterface {
+  handleCoin(total: number): number {
+    return total + 0.25;
+  }
+}
+
+class DimeHandler implements CoinHandlerInterface {
+  handleCoin(total: number): number {
+    return total + 0.10;
+  }
+}
+
+class NickelHandler implements CoinHandlerInterface {
+  handleCoin(total: number): number {
+    return total + 0.05;
+  }
+}
