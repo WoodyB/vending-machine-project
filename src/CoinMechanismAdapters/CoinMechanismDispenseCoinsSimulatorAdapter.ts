fc13dfadf33bf038821b6eff4a9b4cd2a8773b45
Woody Beverley <woodybeverley@gmail.com>
2024-04-15 12:41:50 -0700

    Added test for CoinMechanismDispenseCoinsSimulatorAdapter

--- src/CoinMechanismAdapters/CoinMechanismDispenseCoinsSimulatorAdapter.ts
+++ src/CoinMechanismAdapters/CoinMechanismDispenseCoinsSimulatorAdapter.ts
@@ -1,7 +1,7 @@
 import { CoinMechanismDispenseCoinsInterface } from '../interfaces'
 import { Coins } from '../types';
 import { Terminal } from '../Simulator/Terminal';
-//import { VM_STR_ACTION, VM_STR_COIN_REJECTED } from '../constants/vending-machine-strings';
+import { VM_STR_ACTION, VM_STR_CHANGE_DISPENSED } from '../constants/vending-machine-strings';
 
 export class CoinMechanismDispenseCoinsSimulatorAdapter implements CoinMechanismDispenseCoinsInterface{
   private terminal: Terminal;
@@ -10,8 +10,10 @@ export class CoinMechanismDispenseCoinsSimulatorAdapter implements CoinMechanism
     this.terminal = terminal;
   }
   
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public dispenseCoin(coin: Coins): void {
-    
+    if (coin === Coins.NO_COIN) {
+      return;
+    }
+    this.terminal.output(`${VM_STR_ACTION} ${coin} ${VM_STR_CHANGE_DISPENSED}`);
   }
 }
